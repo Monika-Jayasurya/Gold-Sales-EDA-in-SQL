bcdfe0492eee9333040abb394c696381f5d0d3cb
Monika Jayasurya <monikajayasurya01@gmail.com>
2025-09-14 02:33:41 +0530

    Add files via upload

---
+++ Script/00_init_database.sql
@@ -0,0 +1,115 @@
+﻿/*
+=============================================================
+Create Database and Schemas
+=============================================================
+Script Purpose:
+    This script creates a new database named 'DataWarehouseAnalytics' after checking if it already exists. 
+    If the database exists, it is dropped and recreated. Additionally, this script creates a schema called gold
+	
+*/
+
+
+
+USE master;
+GO
+
+-- Drop and recreate the 'DataWarehouseAnalytics' database
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouseAnalytics')
+BEGIN
+    ALTER DATABASE DataWarehouseAnalytics SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE DataWarehouseAnalytics;
+END;
+GO
+
+-- Create the 'DataWarehouseAnalytics' database
+CREATE DATABASE DataWarehouseAnalytics;
+GO
+
+USE DataWarehouseAnalytics;
+GO
+
+-- Create Schemas
+IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'gold')
+    EXEC('CREATE SCHEMA gold');
+GO
+
+-- Drop tables if already exist (avoid duplicate errors)
+IF OBJECT_ID('gold.dim_customers', 'U') IS NOT NULL DROP TABLE gold.dim_customers;
+IF OBJECT_ID('gold.dim_products', 'U') IS NOT NULL DROP TABLE gold.dim_products;
+IF OBJECT_ID('gold.fact_sales', 'U') IS NOT NULL DROP TABLE gold.fact_sales;
+GO
+
+-- Create Tables
+CREATE TABLE gold.dim_customers(
+	customer_key int,
+	customer_id int,
+	customer_number nvarchar(50),
+	first_name nvarchar(50),
+	last_name nvarchar(50),
+	country nvarchar(50),
+	marital_status nvarchar(50),
+	gender nvarchar(50),
+	birthdate date,
+	create_date date
+);
+GO
+
+CREATE TABLE gold.dim_products(
+	product_key int,
+	product_id int,
+	product_number nvarchar(50),
+	product_name nvarchar(50),
+	category_id nvarchar(50),
+	category nvarchar(50),
+	subcategory nvarchar(50),  -- ✅ moved inside here
+	maintenance nvarchar(50),
+	cost int,
+	product_line nvarchar(50),
+	start_date date
+);
+GO
+
+CREATE TABLE gold.fact_sales(
+	order_number nvarchar(50),
+	product_key int,
+	customer_key int,
+	order_date date,
+	shipping_date date,
+	due_date date,
+	sales_amount int,
+	quantity tinyint,
+	price int
+);
+GO
+
+-- Load Data
+TRUNCATE TABLE gold.dim_customers;
+BULK INSERT gold.dim_customers
+FROM 'C:\SQL2022\Developer_ENU\sql-data-analytics-project-DWB-EDA\datasets\csv-files\gold.dim_customers.csv'
+WITH (
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	ROWTERMINATOR = '\n',
+	TABLOCK
+);
+
+TRUNCATE TABLE gold.dim_products;
+BULK INSERT gold.dim_products
+FROM 'C:\SQL2022\Developer_ENU\sql-data-analytics-project-DWB-EDA\datasets\csv-files\gold.dim_products.csv'
+WITH (
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	ROWTERMINATOR = '\n',
+	TABLOCK
+);
+
+TRUNCATE TABLE gold.fact_sales;
+BULK INSERT gold.fact_sales
+FROM 'C:\SQL2022\Developer_ENU\sql-data-analytics-project-DWB-EDA\datasets\csv-files\gold.fact_sales.csv'
+WITH (
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	ROWTERMINATOR = '\n',
+	TABLOCK
+);
+GO
